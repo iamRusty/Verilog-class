565d50b182da443ae17f73a4af8ffecad10ceb5c
Rusty <rusty.mina@eee.upd.edu.ph>
2018-02-27 00:01:30 +0800

    Change reward controller to FSM

--- main/reward.v
+++ main/reward.v
@@ -4,7 +4,7 @@
 `define WORD_WIDTH 16
 `define CLOCK_PD 20
 
-module reward(clock, nreset, _action, _besthop, address, data_in, data_out, MY_NODE_ID, MY_CLUSTER_ID, done_prev, done);
+module reward(clock, nreset, _action, _besthop, address, data_in, MY_NODE_ID, MY_CLUSTER_ID, done_prev, done, new_data_out);
     input clock, nreset;
     input [`WORD_WIDTH-1:0] _action;
     input [`WORD_WIDTH-1:0] _besthop;
@@ -23,8 +23,6 @@ module reward(clock, nreset, _action, _besthop, address, data_in, data_out, MY_N
      *
      *  Total = 10 bytes or 80 bits
      */
-    output [80 - 1: 0] data_out;
-    reg [80 - 1: 0] data_out_buf;   // data_out buffer
 
     // fsourceID
     input [`WORD_WIDTH-1:0] MY_NODE_ID;
@@ -34,6 +32,7 @@ module reward(clock, nreset, _action, _besthop, address, data_in, data_out, MY_N
 
     input done_prev;
     output done;
+    output [15:0] new_data_out;
 
     // Done buffer
     reg done_buf;
@@ -47,7 +46,6 @@ module reward(clock, nreset, _action, _besthop, address, data_in, data_out, MY_N
     // Negative Reset
     always @ (negedge nreset) begin
         done_buf <= 0;
-        data_out_buf <= 0;
         tick <= 0;
         address_count <= 8;
     end
@@ -57,78 +55,112 @@ module reward(clock, nreset, _action, _besthop, address, data_in, data_out, MY_N
         if (!nreset)
             tick <= 0;
         else
-            tick <= tick + 1;
+            if (state == 0)
+                tick <= 0;
+            else
+                tick <= tick + 1;
+    end
+
+    // Tick more
+    always @ (posedge done_prev) begin
+        tick <= 0;
     end
 
     /*
      *  State Machine
-     *  0 - Wait for done_prev
+     *  0 - IDLE/Wait for done_prev 
      *  1 - Process fsourceID
      *  2 - Process fbatteryStat
      *  3 - Process fValue
      *  4 - Process fclusterID
      *  5 - Process fdestinationID
-     *  6 - Concatenate
-     *  7 - done
+     *  6 - done
      */
-     
+
+    // Next state
     reg [4:0] state;
     always @ (posedge clock) begin
-        if (!nreset)
+        if (!nreset) begin
             state <= 0;
+        end
         else begin
             case(state)
+                4'd0:
+                    if (done_prev)
+                        state <= 1;
+                    else
+                        state <= 0;
+                4'd1:
+                    if (tick < 3)
+                        state <= 1;
+                    else
+                        state <= 2;
+                4'd2:
+                    if (tick < 6)
+                        state <= 2;
+                    else
+                        state <= 3;
+                4'd3:
+                    if (tick < 9)
+                        state <= 3;
+                    else
+                        state <= 4;
+                4'd4:
+                    if (tick < 12)
+                        state <= 4;
+                    else
+                        state <= 5;
+                4'd5:
+                    if (tick < 15)
+                        state <= 5;
+                    else
+                        state <= 6;          
+                default: state <= 6;
+            endcase
         end
     end
 
+    // Done ???
+    always @ (posedge clock) begin
+        if (!nreset) 
+            done_buf = 0;
+        else
+            if (state == 6)
+                done_buf = 1;
+            else
+                done_buf = 0;
+    end
+
+/*
     reg [`WORD_WIDTH-1:0] fsourceID;
     reg [`WORD_WIDTH-1:0] fbatteryStat;
     reg [`WORD_WIDTH-1:0] fValue;
     reg [`WORD_WIDTH-1:0] fclusterID;
     reg [`WORD_WIDTH-1:0] fdestinationID;
+*/
 
-    always @ (posedge done_prev) begin
-        if (done_buf == 0) begin
-            // Get all the components, store in registers
-            // and then concatenate
-            
-            // fsourceID
-            #(`CLOCK_PD);
-            fsourceID = MY_NODE_ID;
-            #(`CLOCK_PD);        
-            
-            // fbatteryStat
-            address_count = 328 + MY_NODE_ID;   // 0x148 + MY_NODE_ID
-            #(`CLOCK_PD);
-            fbatteryStat = data_in;
-            #(`CLOCK_PD);
-            
-            // fValue
-            address_count = 456 + _besthop;     // 0x1C8 + _besthop
-            #(`CLOCK_PD);
-            fValue = data_in;
-            #(`CLOCK_PD);
+    // Output Address
+    reg [15:0] new_data_out_buf;
+    always @ (posedge clock) begin
+        if (state == 0)
+            new_data_out_buf = 0;
+        else
+            case(state)
+                1:  new_data_out_buf = MY_NODE_ID;
+                2:  address_count = 'h148 + MY_NODE_ID*2;
+                3:  address_count = 'h1C8 + _besthop*2;
+                4:  new_data_out_buf = MY_CLUSTER_ID;
+                5:  address_count = 'h48 + _action*2;
+                default: new_data_out_buf = 0;
+            endcase
+    end
 
-            // fclusterID
-            fclusterID = MY_CLUSTER_ID;
-            #(`CLOCK_PD);
+    // Output Data
+    always @ (*)
+        new_data_out_buf = data_in;
 
-            // fdestinationID
-            address_count = 72 + _action;       // 0x48 + _action
-            #(`CLOCK_PD);
-            fdestinationID = data_in;
-            #(`CLOCK_PD);
-
-            // Concatenate
-            data_out_buf = {fsourceID, fbatteryStat, fValue, fclusterID, fdestinationID};
-            #(`CLOCK_PD);
-
-            // Invoke done
-            done_buf = 1;
-        end
-    end 
+    assign new_data_out = new_data_out_buf;
 
     assign address = address_count;
     assign done = done_buf;
-    assign data_out = data_out_buf;
 endmodule
--- main/tb_reward.v
+++ main/tb_reward.v
@@ -18,10 +18,11 @@ module tb_reward();
 
     // REWARD MODULE
     reg [`WORD_WIDTH-1:0] _action, _besthop;
-    wire [80 - 1:0] reward_data_out;
+    //wire [80 - 1:0] reward_data_out;
     reg [`WORD_WIDTH-1:0] MY_NODE_ID, MY_CLUSTER_ID;
     reg done_prev;  //wire done_prev;
     wire done_reward;
+    wire [15:0] new_data_out;
     reward r1(
         clock, 
         nreset, 
@@ -29,11 +30,12 @@ module tb_reward();
         _besthop, 
         address, 
         mem_data_out, 
-        reward_data_out, 
+        //reward_data_out, 
         MY_NODE_ID, 
         MY_CLUSTER_ID, 
         done_prev,      // to be renamed
-        done_reward
+        done_reward,
+        new_data_out
     );
 
     // CLOCK
@@ -45,7 +47,11 @@ module tb_reward();
     // RESET    
     initial begin
         nreset = 0; 
-        #1;
+        #20;
+        nreset = 1;
+        #480
+        nreset = 0;
+        #20
         nreset = 1;
     end
 
@@ -57,14 +63,14 @@ module tb_reward();
         _action <= 5;
         _besthop <= 6;
         
-        #10 done_prev = 1;
+        #100 done_prev = 1;
     end
 
     // DUMP and MAIN SETTINGS
     initial begin
         $dumpfile("tb_reward.vcd");
         $dumpvars(0, tb_reward);
-        #400;
+        #500;
         done_prev = 0;
         #20;
         $finish; 
