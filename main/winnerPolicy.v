4c1482dd7e06afd6de590d23c8093f0dbf7b1028
Rusty <rusty.mina@eee.upd.edu.ph>
2018-02-27 12:31:12 +0800

    Finisehd garbage winnerPolicy ~ Missing float operations and RNG

--- main/winnerPolicy.v
+++ main/winnerPolicy.v
@@ -77,14 +77,111 @@ module winnerPolicy(
     // (COMBINATIONAL) Compare explore_constant and epsilon
     // Already taken care of by FSM
 
-    // (COMBINATIONAL) call_fSub Generator
+    // state12_reg
+    reg [15:0] state12_reg;
+    always @ (*) begin
+        if (state == 13)
+            state12_reg = fSub_data_out;
+        else
+            state12_reg = 0;
+    end
+
+    // state15_reg_sub
+    reg [15:0] state15_reg_sub;
+    reg [15:0] state15_reg_add;
+    always @ (*) begin
+        if (state == 15) begin
+            state15_reg_sub = 1; // Output of floatSub
+            state15_reg_add = 1; // Output of floatAdd
+        end
+        else begin
+            state15_reg_sub = 1; // Arbitrary
+            state15_reg_add = 1; // Arbitrary
+        end
+    end
+
+    // Simplifying the long if
+    reg one, two, three;
     always @ (*) begin
         case (state)
-            2:  call_fSub <= 1;
+            16: begin
+                if (_bestneighborID != MY_NODE_ID)
+                    three = 1;
+                else
+                    three = 0;
 
+                if (sub_compare)
+                    one = 0;
+                else
+                    one = 1;
+            end
+            17: 
+                if (sub_compare)
+                    two = 1;
+                else
+                    two = 0;
+            default: begin
+                one = 0;
+                two = 0;
+                three = 0;
+            end
+        endcase
+    end
+
+    reg inside;
+    always @ (*) begin
+        if (one & two & three)
+            inside = 1;
+        else
+            inside = 0;
+    end
+
+    // (COMBINATIONAL) call_fSub Generator and Arguments
+    always @ (*) begin
+        case (state)
+            2:  begin
+                left = explore_constant;
+                right = epsilon_buf;
+                call_fSub = 1;
+            end
+            7:  begin
+                left = epsilon_buf;
+                right = epsilon_step;
+                call_fSub = 1;
+            end
+            10: begin
+                left = nexthop_buf;
+                right = 0;
+                call_fSub = 1;
+            end
+            12: begin
+                left = _mybest;
+                right = _mybest * 1; // floatMult(_mybest * 0.001)
+                call_fSub = 1;
+            end
+            13: begin
+                left = _bestvalue;
+                right = state12_reg;
+                call_fSub = 1;
+            end
+            15: begin
+                left = _mybest;
+                right = _mybest * 1; // floatMult(_myest * 0.001)
+                call_fSub = 1;
+                // call_fAdd;
+            end
+            16: begin
+                left = _bestvalue;
+                right = state15_reg_sub;
+                call_fSub = 1;
+            end
+            17: begin
+            end
             default: call_fSub <= 0;
         endcase
     end
+
+
 
     // NOT NOT NOT NOT NOT NOT NOT NOT NOT NOT NOT NOT
     // NOT NOT NOT NOT NOT NOT NOT NOT NOT NOT NOT NOT
@@ -102,16 +199,18 @@ module winnerPolicy(
         end
     end
 
+
     // SEEMS WRONG
     // (COMBINATIONAL) epsilon -= epsilon_step
     reg [15:0] epsilon_temp, epsilon_buf;
+/*
     always @ (*) begin
         if (state == 7)
             epsilon_temp = epsilon_buf - epsilon_step;  // Replace with floatSub
         else
             epsilon_temp <= 0;
     end
-
+*/
 
 
     reg [7:0] tick;
@@ -152,8 +251,10 @@ module winnerPolicy(
                     else
                         state <= 3;
                 3:  // Wait for floatSub
-                    if (tick < 9)
+                    if (tick < 9) begin
                         state <= 3;
+                        call_fSub <= 0;
+                    end
                     else
                         state <= 4;
                 4:  // Make decision for 1st if
@@ -177,8 +278,10 @@ module winnerPolicy(
                     else
                         state <= 8;
                 8:  // wait for floatSub
-                    if (tick < 24)
+                    if (tick < 24) begin
                         state <= 8;
+                        call_fSub <= 0;
+                    end
                     else
                         state <= 9;
                 9:  // if (epsilon < 0)
@@ -196,23 +299,26 @@ module winnerPolicy(
                 11: // if (nexthop < 0)
                     if (sub_compare == 1) begin
                         state <= 12;
+                        call_fSub <= 0;
                     end
                     else
-                        state <= 18;
+                        state <= 19;
                 12: // _mybest - _mybest * 0.001
                     if (tick < 33) 
                         state <= 12;
                     else
                         state <= 13;
-                13: // fSub
-                    if (tick < 36)
+                13: // wait for fSub
+                    if (tick < 36) begin
                         state <= 13;
+                        call_fSub <= 0;
+                    end
                     else
                         state <= 14;
                 14: // if (_bestvalue < (_mybest - _mybest * 0.001))
                     if (sub_compare == 1) begin
                         nexthop_buf = _besthop;
-                        state <= 18; 
+                        state <= 19; 
                     end
                     else
                         state <= 15;
@@ -231,9 +337,18 @@ module winnerPolicy(
                         state <= 17;
                     else
                         state <= 18; // done
+                18: // Pinakahuling if
+                    if (inside) begin
+                        nexthop_buf = _besthop;
+                        state <= 19;
+                    end
+                    else 
+                        state <= 19; 
                 default:
-                    state <= 18;    // done
+                    state <= 19;    // done
             endcase
         end
     end
+
+    assign done = (state == 19) ? 1:0;
 endmodule
