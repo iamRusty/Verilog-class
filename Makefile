f9b2739fe62fad17e78e4f6c8376810dc4299446
Rusty <rusty.mina@eee.upd.edu.ph>
2018-03-11 14:11:34 +0800

    Major housekeeping

--- Makefile
+++ Makefile
@@ -1,14 +1,50 @@
-a:
-	iverilog -o costEvaluation costEvaluation.v
-	vvp costEvaluation
-	gtkwave costEvaluation.vcd
-mem:
-	rm -f mem
-	iverilog -o mem mem.v
-	vvp mem
-	gtkwave mem.vcd
+r:
+	iverilog -o tb_reward tb_reward.v
+	vvp tb_reward
+	gtkwave tb_reward.vcd
+
+wp:
+	iverilog -o tb_winnerPolicy tb_winnerPolicy.v
+	vvp tb_winnerPolicy
+	gtkwave tb_winnerPolicy.vcd
+
+wp2:
+	iverilog -o testbench2 testbench2.v
+	vvp testbench2
+	gtkwave testbench2.vcd
+
+rng:
+	iverilog -o tb_randomGenerator tb_randomGenerator.v
+	vvp tb_randomGenerator
+	gtkwave tb_randomGenerator.vcd
+
+rnga:
+	iverilog -o tb_rngAddress tb_rngAddress.v
+	vvp tb_rngAddress
+	gtkwave tb_rngAddress.vcd
+
+lc:
+	iverilog -o tb_learnCosts tb_learnCosts.v
+	vvp tb_learnCosts
+	gtkwave tb_learnCosts.vcd	
+
+s:
+	iverilog -o tb_selectMyAction tb_selectMyAction.v
+	vvp tb_selectMyAction
+	gtkwave tb_selectMyAction.vcd	
+
 clean:
-	rm -f *.vcd *.vpd
 	rm -f a.out
-	rm -f costEvaluation
+	rm -f tb_reward
+	rm -f tb_reward.vcd
+	rm -f tb_randomGenerator.vcd
+	rm -f tb_randomGenerator
+	rm -f tb_rngAddress
+	rm -f tb_rngAddress.vcd
+	rm -f tb_winnerPolicy.vcd
+	rm -f tb_winnerPolicy
+	rm -f tb_learnCosts.vcd
+	rm -f tb_learnCosts
+	rm -f tb_selectMyAction.vcd
+	rm -f tb_selectMyAction
 
--- winnerPolicy.v
+++ winnerPolicy.v
@@ -1,50 +1,217 @@
-module winnerPolicy(clock, reset, value);
-    input clock, reset;
-    output [31:0] value;
+`timescale 1ns/1ps
+`define MEM_DEPTH  2048
+`define MEM_WIDTH  8
+`define WORD_WIDTH 16
+`define CLOCK_PD 20
 
-    reg [31:0] a, b, c;
-    reg [5:0] count;
+module winnerPolicy(
+	clock, 
+	nrst, 
+	en,
+	start_winnerPolicy,
+	mybest,
+	besthop,
+	bestvalue,
+	bestneighborID,
+	MY_NODE_ID,
+	address,
+	data_in,
+	wr_en,
+	data_out,
+	epsilon_step,
+	nexthop,
+	done_winnerPolicy,
+	en_rng,
+	rng_out,
+	rng_out_4bit,
+	rng_address,
+	start_rngAddress,
+	done_rng_address,
+	betterNeighborCount,
+	which
+);
 
-    // Reset the circuit
-    always @ (posedge reset)
-    begin
-        a <= 31'd0;
-        b <= 31'd1;
-        count <= 6'd0;
-    end
+	input clock, nrst, en, start_winnerPolicy, done_rng_address;
+	input [`WORD_WIDTH-1:0] mybest, besthop, bestvalue, bestneighborID, MY_NODE_ID, data_in, epsilon_step, rng_out, rng_out_4bit, rng_address;
+	output [`WORD_WIDTH-1:0] address, data_out, nexthop, betterNeighborCount, which;
+	output wr_en, done_winnerPolicy, en_rng, start_rngAddress;
 
-    // Every clock
-    always @ (posedge clock)
-    begin 
-        a = a * 2;
-        count = count + 1;
-    end
+	// Registers
+	reg [`WORD_WIDTH-1:0] explore_constant, which_buf, address_count, epsilon_buf, nexthop_buf, data_out_buf; 
+	reg [`WORD_WIDTH-1:0] betterNeighborCount_buf, rng_address_temp;
+	reg wr_en_buf, done_winnerPolicy_buf, en_rng_buf, start_rngAddress_buf;
+	reg one, two, three;
+	reg [15:0] nineninenine, onezerozeroone;
+	reg [31:0] _right3, _right2, _left, _right;
+	reg [31:0] mybest_shifted;
 
-    // Read the value of a and assign to value
-    assign value = a;
-endmodule
+	reg [4:0] state;
+	always @ (posedge clock) begin
+		if (!nrst) begin
+			state <= 11;	// Idle State
+			wr_en_buf <= 0;
+			en_rng_buf <= 0;
+			done_winnerPolicy_buf <= 0;
+			nexthop_buf <= 100;     // 100 = -1 for the lack of representation on negative numbers
+			start_rngAddress_buf <= 0;
+			nineninenine <= 16'b1111111110111110;   	// 0.999 in binary fraction ~ 0.998992919921875 0.16
+			onezerozeroone <= 16'b1000000000100000;    	// 0.001 in binary fraction ~ 0.001007080078125	1.15
+		end
+		else begin
+			case (state)
+				5'd0: begin
+					if (start_winnerPolicy) begin
+						state <= 14;
+						en_rng_buf <= 1; // Generate RNG (explore_constant)
+					end
+					else state <= 0;
+				end
+				5'd14: begin
+					explore_constant <= rng_out_4bit;
+					en_rng_buf <= 0;
+					address_count <= 16'h4; // epsilon address
+					state <= 13;
+				end
+				5'd13: begin
+					epsilon_buf <= data_in;
+					state <= 1;
+				end
+				5'd1: begin
+					if (explore_constant < epsilon_buf) begin
+						state <= 2;
+						en_rng_buf <= 1; // Generate RNG (which)
+						address_count <= 16'h68C; // betterNeighborCount Address
+					end
+					else begin
+						// nexthop is less than 0, or explore_constant is less than epsilon
+						// Pick the best instead of exploring
+						state <= 5;
+					end
+				end
+				5'd2: begin
+					en_rng_buf <= 0;
+					which_buf <= rng_out_4bit;
+					betterNeighborCount_buf <= data_in;
+					
+					// Compute for the address of betterNeighbor
+					start_rngAddress_buf <= 1;
+					state <= 3;
+				end
+				5'd3: begin
+					if (done_rng_address) begin
+						state <= 4;
+						start_rngAddress_buf <= 0;
+						rng_address_temp = rng_address;
 
-module winnerPolicy_tb();
-    reg clock, reset;
-    wire [31:0] value;
+						// address and index of betterNeighbor
+						address_count <= 16'h668 + rng_address_temp*2;
+					end
+					else 
+						state <= 3;
+				end
+				5'd4: begin
+					nexthop_buf <= data_in;
+					if (epsilon_buf < epsilon_step)
+						data_out_buf <= 0;
+					else
+						data_out_buf <= epsilon_buf - epsilon_step;
+					
+					// Write epsilon value to memory
+					$display("epsilon: %d", data_out_buf);
+					address_count <= 16'h4;
+					wr_en_buf <= 1;
+					state <= 12;
+				end
+				5'd5: begin            
+					/*
+					 *  [15:0] bestvalue   - 11 bits whole, 5 bits fraction
+					 *  [15:0] mybest      - 11 bits whole, 5 bits fraction
+					 *  [15:0] nineninenine  - 16 bits fraction
+					 *  _left, _right = 11 bits whole, 21 bits fraction
+					 */
+					_left = {bestvalue, 16'b0};
+					_right = mybest * nineninenine;
+					state <= 6;
+				end
+				5'd6: begin
+					// Malayong mas malaki ang mybest kaysa bestvalue 
+					if (_left < _right) begin
+						one <= 0;
+						nexthop_buf <= besthop;
 
-    winnerPolicy wp1(clock, reset, value);
+						// Done winnerPolicy
+						state <= 10;
+					end
+					else begin
+						one <= 1;
+						state <= 7;
+					end
+				end
+				5'd7: begin
+					/*
+					 *  [15:0] bestvalue   - 11 bits whole, 5 bits fraction
+					 *  [15:0] mybest      - 11 bits whole, 5 bits fraction
+					 *  [15:0] onezerozeroone - 1 bit whole, 15 bits fraction
+					 *  _left, _right2 = 12 bits whole, 20 bits fraction
+					 */
+					_right2 = mybest * onezerozeroone; // 12 bits whole, 20 bits fraction 
+					mybest_shifted = {mybest, 15'b0};
+					_right3 = _right2 + mybest_shifted;
+					state <= 8;
+				end
+				5'd8: begin
+					if (_left < _right3) 
+						two <= 1;
+					else
+						two <= 0;
 
-    initial begin
-        reset = 1;
-        #10 reset = 0;
-        #100 reset = 1;
-        #100 reset = 0;
-    end
+					if (bestneighborID == MY_NODE_ID)
+						three <= 0;
+					else
+						three <= 1;
 
-    initial begin
-        clock = 0;
-        forever #10 clock = ~clock;
-    end
+					state <= 9;
+				end
+				5'd9: begin
+					if (one & two & three) begin
+						nexthop_buf <= besthop;
+					end
+						state <= 10;
+				end
+				5'd10: begin
+					$display("epsilon: %d", epsilon_buf);
+					done_winnerPolicy_buf <= 1;
+					state <= 11;
+				end
+				5'd11: begin
+					if (en) begin
+						state <= 0;
+						done_winnerPolicy_buf <= 0;
+						en_rng_buf <= 0;
+						nexthop_buf <= 301;     // 301 = -1 for the lack of representation for negative numbers
+						start_rngAddress_buf <= 0;
+						nineninenine <= 16'b1111111110111110;   	// 0.999 in binary fraction ~ 0.998992919921875 0.16
+						onezerozeroone <= 16'b1000000000100000;    	// 0.001 in binary fraction ~ 0.001007080078125	1.15
+					end
+					else state <= 11;
+				end
+				5'd12: begin
+					wr_en_buf <= 0;
+					state <= 10; // Done winnerPolicy
+				end
+				default
+					state <= 11;                 
+			endcase
+		end
+	end
 
-    initial begin
-        $dumpfile("winnerPolicy.vcd");
-        $dumpvars(0, winnerPolicy_tb);
-        #200 $finish;
-    end
+	assign nexthop = nexthop_buf;
+	assign data_out = data_out_buf;
+	assign done_winnerPolicy = done_winnerPolicy_buf;
+	assign address = address_count;
+	assign start_rngAddress = start_rngAddress_buf;
+	assign betterNeighborCount = betterNeighborCount_buf;
+	assign which = which_buf;
+	assign wr_en = wr_en_buf;
+	assign en_rng = en_rng_buf;
 endmodule
