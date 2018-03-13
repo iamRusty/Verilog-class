a9fbd4a061785e224c626d55323f5affafa0db3a
Rusty <rusty.mina@eee.upd.edu.ph>
2018-03-13 13:12:29 +0800

    learnCosts 11-bit address

--- tb_learnCosts.v
+++ tb_learnCosts.v
@@ -12,7 +12,8 @@ module tb_learnCosts();
 
 	// Memory Module
 	wire wr_en;
-	wire [`WORD_WIDTH-1:0] address, mem_data_in, mem_data_out;
+	wire [`WORD_WIDTH-1:0] mem_data_in, mem_data_out;
+    wire [10:0] address;
 	mem mem1(clock, address, wr_en, mem_data_in, mem_data_out);
 
 	// learnCosts Module
